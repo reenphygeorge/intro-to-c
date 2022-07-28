5861cf01acc7fbaceecd5340627fb59518c17fda
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-07-28 22:34:52 +0530

    Create 02_user_input.c

---
+++ 02_user_input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    
+int main()
+{
+    int i;
+    printf("Enter the value of i");
+    
+    // user input happens here //
+    scanf("%d",&i);
+   
+    printf("%d",i);
+    return 0;
+}
