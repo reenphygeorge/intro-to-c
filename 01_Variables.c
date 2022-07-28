0a208b34fc5e9193e830063159dc9332b103e423
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-07-28 22:32:53 +0530

    Create 01_Variables.c

---
+++ 01_Variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+    
+int main()
+{
+    // for integer values //
+    int j = 45;
+    printf("%d",j);
+  
+    // for decimal values //
+    float f = 45.4;
+    printf("%f",f);
+  
+    // for large decimal values //
+    double d = 1234.568790;
+    printf("%f",d);
+
+    // for character values //
+    char ch = 'y';
+    printf("%c",ch);
+  
+    return 0;
+}
