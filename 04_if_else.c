efbabaaf79cba744c2bf0fd0c6199f74b8ef568b
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-07-28 22:51:15 +0530

    Create 04_if_else.c

---
+++ 04_if_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    
+int main()
+{
+    int value;
+    scanf("%d",&value);
+    
+    if(value == 8)
+    {
+        printf("Value is equal to 8");
+    }
+    else
+    {
+        printf("Value is not equal to 8");
+    }
+    return 0;
+}
