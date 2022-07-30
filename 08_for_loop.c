caa2da1969ac3474f2596f0acaa18605a0af1a28
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-07-30 09:12:02 +0530

    Create 08_for_loop.c

---
+++ 08_for_loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+      for(int i=0; i<5; i++)
+      {
+          printf("Hello\n");
+      }
+      
+      // Steps involved in for loop //
+      // 1st step: i = 0
+      // 2nd step: is i<5 ??
+      // 3 step : printf();
+      // 4 step : i++ => i = i+1
+      // 2nd step continues until condition in 2nd step goes wrong
+  
+      return 0;
+}
