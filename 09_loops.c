ba380e77eabe52d8355758134080c12b4293295b
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-08-01 23:21:58 +0530

    Create 09_loops.c

---
+++ 09_loops.c
@@ -0,0 +1,26 @@
+#include<stdio.h>    
+
+int main()
+{
+    // while Loop //
+    int i=0;
+    while(i<5)
+    {
+        printf("%d",i);
+    }
+    
+    // Do-while Loop //
+    int j=0;  
+    do
+    {
+        printf("%d",j);
+    }while(j<5);
+    
+    // For Loop //
+    for(int k=0; k<5; k++)
+    {
+      printf("%d",k);
+    }
+  
+    return 0;
+}
